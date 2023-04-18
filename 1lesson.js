547e228cdb326eec4c7163e195d24f25e939de88
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-04-18 20:34:34 +0700

    урок1

---
+++ 1lesson.js
@@ -0,0 +1,7 @@
+let name = 'Жека';
+let occupation = 'Ювелирный мастер';
+let yearOfBirth = 1994;
+let thisYear = 2023;
+let dateOfBirth = thisYear - yearOfBirth ;
+let story = `Меня зовут ${name}, мне ${dateOfBirth} лет. Последне 8 лет я работал по професии ${occupation}.`;
+console.log(story);
